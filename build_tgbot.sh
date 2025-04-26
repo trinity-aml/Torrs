80c40d63ec1e33fba7e248ce16f5373086acaabe
trinity-aml <trinity1980@fex.net>
2025-04-26 12:27:04 +0300

    update

--- build_tgbot.sh
+++ build_tgbot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
-git clone --recursive https://github.com/tdlib/telegram-bot-api.git tgbotapi
-mkdir tgbotapi/build
-cd tgbotapi/build
+##rm -fr ./tgbotapi
+##git clone --recursive https://github.com/tdlib/telegram-bot-api.git tgbotapi
+cd ./tgbotapi
+rm -fr ./build
+mkdir ./build
+cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j8
-cp telegram-bot-api ../../dist
+cmake --build .
+cp ./telegram-bot-api ../../dist
--- version/version.go
+++ version/version.go
@@ -1,3 +1,3 @@
 package version
 
-const Version = "Torrs.1.0.4"
+const Version = "Torrs.1.0.5"
